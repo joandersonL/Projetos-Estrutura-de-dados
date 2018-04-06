b33e21ae038901a0496b59e20e9ead765b777d23
wps13 <willanepaiva@hotmail.com>
2018-04-06 10:20:25 -0300

    Completed almost all methods

--- PraticaMOD4/CalcRPN.java
+++ PraticaMOD4/CalcRPN.java
@@ -15,21 +15,69 @@ public class CalcRPN {
     }
 
     void menos() {
-        throw new Error("Must be completed");
+        double a = aPilha.desempilha;
+        double b = aPilha.desempilha;
+        aPilha.empilha(b - a);
+        hist.empilha(new Operacao(a, b, '-'));
     }
 
     void vezes() {
-        throw new Error("a ser completado");
+        double a = aPilha.desempilha;
+        double b = aPilha.desempilha;
+        aPilha.empilha(b * a);
+        hist.empilha(new Operacao(a, b, '*'));
     }
 
     void dividido() {
-        throw new Error("a ser completado");
+        double a = aPilha.desempilha;
+        double b = aPilha.desempilha;
+        aPilha.empilha(b / a);
+        hist.empilha(new Operacao(a, b, '/'));
+    }
+
+    // interpretador de comandos
+    void exec(String cmd) {
+        double valor;
+
+        try {
+            valor = (Double.parseDouble(cmd));
+            aPilha.empilha(valor);
+            hist.empilha(new Operacao(valor));
+
+        } catch (NumberFormatException e) {
+            if (null == cmd) {
+                throw new Error("");
+
+            } else
+                switch (cmd) {
+                case "+":
+                    mais();
+                    break;
+
+                case "-":
+                    menos();
+                    break;
+
+                case "*":
+                    vezes();
+                    break;
+
+                case "/":
+                    dividido();
+                    break;
+
+                case "undo":
+                    break;
+
+                default:
+                    throw new Error("");
+                }
+
+        }
     }
 
     // retorna o conteudo do topo da pilha
     Double resultado() {
         throw new Error("a ser completado");
-        }
-    // interpretador de comandosvoid exec(String cmd) {
-    throw new Error("a ser completado");
-}}}
+    }
+}
