376dd812485fc2f2918a0da1b74a24e90aab53be
wps13 <willanepaiva@hotmail.com>
2018-04-06 09:34:33 -0300

    Exec test1

--- PraticaMOD4/Pilha.java
+++ PraticaMOD4/Pilha.java
@@ -11,11 +11,39 @@ public class Pilha<T> {
         conteudo.addFirst(novoDado);
     }
 
-    public void desempilha() {
-
+    public T desempilha() {
+       return conteudo.removeFirst();
     }
 
     public T topo() {
+        return conteudo.getFirst();
+    }
 
+
+
+    static void test1() {
+        Pilha<Double> aPilha = new Pilha<Double>();
+        aPilha.empilha(1.1);
+        aPilha.empilha(2.1);
+        aPilha.empilha(3.1);
+        aPilha.empilha(4.1);
+        aPilha.empilha(5.1);
+        double valor = 0.0;
+        valor = aPilha.topo();
+        System.out.println("topo pilha = " + valor);
+        valor = aPilha.desempilha();
+        System.out.println("topo pilha = " + valor);
+        valor = aPilha.desempilha();
+        System.out.println("topo pilha = " + valor);
+        valor = aPilha.desempilha();
+        System.out.println("topo pilha = " + valor);
+        valor = aPilha.topo();
+        System.out.println("topo pilha = " + valor);
+        valor = aPilha.desempilha();
+        System.out.println("topo pilha = " + valor);
+    }
+
+    public static void main(String[] args) {
+    test1();
     }
 }
