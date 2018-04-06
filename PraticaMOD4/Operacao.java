ffd888a9d5be7223c595090bccbedd2a377aca42
wps13 <willanepaiva@hotmail.com>
2018-04-06 09:46:05 -0300

    class Operacao.java

---
+++ PraticaMOD4/Operacao.java
@@ -0,0 +1,11 @@
+public class Operacao{
+    private double a,b;
+    private char code;
+
+    public Operacao(double a,double b, char code){
+        this.a=a;
+        this.b=b;
+        this.code = code;
+    }
+    
+}
