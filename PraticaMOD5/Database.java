a7021baec7fd4aba5cc97b0c5eeff1a85dfd14b6
wps13 <willanepaiva@hotmail.com>
2018-04-20 10:30:37 -0300

    changes

--- PraticaMOD5/Database.java
+++ PraticaMOD5/Database.java
@@ -5,14 +5,16 @@ public class Database{
 
     static{
         caracteres = new ArrayList<Letra>();
-        caracteres.add(new Letra("a"));
+        /* caracteres.add(new Letra("a"));
         caracteres.add(new Letra("b"));
 
         caracteres.add(new Letra("A"));
         caracteres.add(new Letra("B"));
 
         caracteres.add(new Letra("0"));
+
+        caracteres.add(new Letra(" ")); */
+
         
-        caracteres.add(new Letra(" "));
     }
 }
