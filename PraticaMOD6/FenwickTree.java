55da62302b240fea2681df0501c89502b24c0b47
wps13 <willanepaiva@hotmail.com>
2018-05-04 11:00:50 -0300

    teste allZeros

--- PraticaMOD6/FenwickTree.java
+++ PraticaMOD6/FenwickTree.java
@@ -37,7 +37,7 @@ public class FenwickTree {
             return "[" + Integer.toString(value)  + "," + Integer.toString(leftSize) + ","+ left.toString() + "]";
 
         else
-            return "[" + Integer.toString(value)  + "," + Integer.toString(leftSize)+ "," + left.toString() + ","+ right.toString()+ "]";
+            return "[" + Integer.toString(value)  + "," + Integer.toString(leftSize)+ "," + left.toString() + ","+ right.toString() + "]";
     }
 
     static FenwickTree allZeros(int n) {
@@ -49,15 +49,17 @@ public class FenwickTree {
         return new FenwickTree(0, n - m, allZeros(n - m), allZeros(m));
     }
 
-    /*
-     * public int size() { // retorna num de folhas }
-     * 
-     * public int prefixSum(int upto) { // soma dos valores das folhas, 0 ≤ upto < n
-     * }
-     * 
-     * public int between(int lo, int hi) { // soma dos valores das folhas, 0 ≤ lo ≤
-     * hi ≤ n, }
-     */
+    public int size() { // retorna num de folhas
+        return 0;
+    }
+
+    public int prefixSum(int upto) { // soma dos valores das folhas, 0 ≤ upto < n
+        return 0;
+    }
+
+    public int between(int lo, int hi) { // soma dos valores das folhas, 0 ≤ lo ≤ hi ≤ n, 
+        return 0;
+    }
 
     public void increment(int i, int delta) {
         value += delta;
@@ -67,10 +69,18 @@ public class FenwickTree {
             right.increment(i - leftSize, delta);
     }
 
+    /*
+     * public static void main(String[] args) {
+     * System.out.println("Construcao de FenwickTree(3) : " + new FenwickTree(3));
+     * System.out.println("Construcao da arvore da figura : " + new FenwickTree(3,
+     * new FenwickTree(1, new FenwickTree(4), new FenwickTree(1, new FenwickTree(2),
+     * new FenwickTree(5))), new FenwickTree(1, new FenwickTree(3), new
+     * FenwickTree(1, new FenwickTree(6), new FenwickTree(1))))); }
+     */
     public static void main(String[] args) {
-        System.out.println("Construcao de FenwickTree(3) : " + new FenwickTree(3));
-        System.out.println("Construcao da arvore da figura : " + new FenwickTree(3,
-                new FenwickTree(1, new FenwickTree(4), new FenwickTree(1, new FenwickTree(2), new FenwickTree(5))),
-                new FenwickTree(1, new FenwickTree(3), new FenwickTree(1, new FenwickTree(6), new FenwickTree(1)))));
+        System.out.println("Construcao de allZeros(3) :" + FenwickTree.allZeros(3));
+        System.out.println("Construcao de allZeros(4):" + FenwickTree.allZeros(4));
+        System.out.println("Construcao de allZeros(5):" + FenwickTree.allZeros(5));
+        System.out.println("Construcao de allZeros(6):" + FenwickTree.allZeros(6));
     }
 }
