9057756b63a5e1ddb0483d10293f76ae4e9a7936
wps13 <willanepaiva@hotmail.com>
2018-05-06 14:47:20 -0300

    continuação Classe Letra

--- PraticaMOD5/Letra.java
+++ PraticaMOD5/Letra.java
@@ -1,16 +1,31 @@
 public class Letra {
-    String caracteres;
-    String md5Code;
+    private String caractere, md5Code;
 
-    Letra(String caracteres) {
-
+    Letra(String caractere) {
+        this.caractere = caractere;
+        this.md5Code = SecurityProvider.md5(caractere);
     }
 
-    Letra getLetra(String md5){
-        for(caracteres){
-          if(caracteres.get(i).getMd5().equals(md5)) 
-          return caracteres.get(i);
-        }
-        return null;
+    public String getCaractere() {
+        return caractere;
+    }
+
+    public String getMd5Code() {
+        return md5Code;
+    }
+
+    /*
+     * public static Letra getLetra(String md5){ for(caracteres){
+     * if(caracteres.getCaractere(i).getMd5().equals(md5)) return
+     * caracteres.getCaractere(i); } return null; }
+     */
+    public static void test6() {
+        Letra l = new Letra("a");
+        String md5 = l.getMd5Code();
+        System.out.println(md5);
+    }
+
+    public static void main(String[] args) {
+        test6();
     }
 }
