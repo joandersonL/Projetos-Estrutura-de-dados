5c74bdc3995af0d66703c452d3a89038ca467473
wps13 <willanepaiva@hotmail.com>
2018-05-11 10:21:56 -0300

    getConta Finalizado

--- PraticaMOD5/Database.java
+++ PraticaMOD5/Database.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 
-public class Database{
+public class Database {
     public static final ArrayList<Letra> caracteres;
 
-    static{
+    static {
         caracteres = new ArrayList<Letra>();
         char a = 'a';
-        
-        for(int i=0;i<26;i++){
-           caracteres.add(new Letra(Character.toString((char) (a+i))));
+
+        for (int i = 0; i < 26; i++) {
+            caracteres.add(new Letra(Character.toString((char) (a + i))));
         }
-        char a = 'A';
-        for(int i=0;i<26;i++){
-           caracteres.add(new Letra(Character.toString((char) (a+i))));
+        a = 'A';
+        for (int i = 0; i < 26; i++) {
+            caracteres.add(new Letra(Character.toString((char) (a + i))));
         }
-        char a='0';
-        for(int i=0;i<10;i++){
-           caracteres.add(new Letra(Character.toString((char) (a+i))));
+        a = '0';
+        for (int i = 0; i < 10; i++) {
+            caracteres.add(new Letra(Character.toString((char) (a + i))));
         }
         caracteres.add(new Letra(" "));
-        
+
     }
     /*
      * public static Letra getLetra(String md5){ for(caracteres){
--- PraticaMOD5/ServerDatabase.java
+++ PraticaMOD5/ServerDatabase.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.math.BigInteger;
+
 public class ServerDatabase extends Database {
     public static final ArrayList<ArrayList<Conta>> contas;
     public static final int N = 100;
@@ -18,11 +21,11 @@ public class ServerDatabase extends Database {
 
     public static void insereConta(Conta conta) {
 
-        int indice = hashCode(md5);
+        int indice = hashCode(conta.getMD5());
         ArrayList<Conta> posicao = contas.get(indice);
 
-        for (i = 0; i < posicao.size(); i++) {
-            if (posicao.get(i).getMD5.equals(conta.getMD5())) {
+        for (int i = 0; i < posicao.size(); i++) {
+            if (posicao.get(i).getMD5().equals(conta.getMD5())) {
                 return;
             }
         }
@@ -30,5 +33,28 @@ public class ServerDatabase extends Database {
     }
 
     public static Conta getConta(String md5) {
+
+        int indice = hashCode(md5);
+        ArrayList<Conta> posicao = contas.get(indice);
+
+        for (int i = 0; i < posicao.size(); i++) {
+            if (posicao.get(i).getMD5().equals(md5)) {
+                return posicao.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static void test3() {
+        Conta c = new Conta("1234", "2222", "1245");
+        ServerDatabase.insereConta(c);
+        String chave = SecurityProvider.md5ToServer(c);
+        System.out.println(chave);
+        Conta conta = ServerDatabase.getConta(chave);
+        System.out.println(conta);
+    }
+
+    public static void main(String[] args) {
+        test3();
     }
 }
